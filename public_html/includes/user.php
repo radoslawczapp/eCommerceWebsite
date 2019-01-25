b3cda085aa14c13a7362d413135cce6a01d8f93b
radoslawczapp <czappradoslaw@gmail.com>
2019-01-25 22:01:27 +0100

    User last_login, session

--- public_html/includes/user.php
+++ public_html/includes/user.php
@@ -49,11 +49,42 @@ class User{
         }
 
     }
+
+    public function userLogin($email, $password){
+        $pre_stmt = $this->con->prepare("SELECT id, username, password, last_login FROM users WHERE email = ?");
+        $pre_stmt->bind_param("s", $email);
+        $pre_stmt->execute() or die($this->con->error);
+        $result = $pre_stmt->get_result();
+
+        if ($result->num_rows < 1) {
+            return "NOT REGISTERED";
+        } else {
+            $row = $result->fetch_assoc();
+            if (password_verify($password, $row['password'])) {
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['last_login'] = $row['last_login'];
+
+                $last_login = date('Y-m-d H:i:s');
+
+                $pre_stmt = $this->con->prepare("UPDATE users SET last_login = ? WHERE email = ?");
+                $pre_stmt->bind_param('ss', $last_login, $email);
+                $result = $pre_stmt->execute() or die($this->con->error);
+                if ($result) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            } else {
+                return "PASSWORD_NOT_MATCHED";
+            }
+        }
+    }
 }
-
 $user = new User();
-echo $user->createUserAccount("John", "johndoe@gmail.com", "1234567890", "Admin");
+// echo $user->createUserAccount("Jane Doe", "janedoe@gmail.com", "0987654321", "Admin");
 
+echo $user->userLogin("janedoe@gmail.com", "0987654321");
 
-
+echo $_SESSION['username'];
  ?>
