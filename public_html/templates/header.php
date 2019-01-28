f5989aa7d1a92e6ec749b26bd839005de4a0cfcd
radoslawczapp <czappradoslaw@gmail.com>
2019-01-28 20:22:20 +0100

    Header

--- public_html/templates/header.php
+++ public_html/templates/header.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><i class="fa fa-film">&nbsp;</i>Inventory System</a>
+    <a class="navbar-brand" href="index.php"><i class="fa fa-film">&nbsp;</i>Inventory System</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#"><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php"><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
             </li>
             <?php if (isset($_SESSION["user_id"])) { ?>
                 <li class="nav-item active">
